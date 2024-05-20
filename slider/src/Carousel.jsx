b308481d0dd7426cbba9aa477df3cb8aa37f7f5d
anujks1004 <anuj.singh100420@gmail.com>
2024-05-21 01:46:58 +0530

    Carousel added and next, prev btn working

---
+++ slider/src/Carousel.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { shortList, list, longList } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Carousel = () => {
+  const [people, setPeople] = useState(list);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
+  const prevSlide = () => {
+    setCurrentPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  return (
+    <section className="slider-container">
+      {people.map((person, personIdx) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIdx - currentPerson)}%)`,
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+      <button className="prev" type="button" onClick={prevSlide}>
+        <FiChevronLeft />
+      </button>
+      <button className="next" type="button" onClick={nextSlide}>
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+export default Carousel;
