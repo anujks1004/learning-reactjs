e90e887861e3f94b3b723d33a38ceaa5672df5ec
anujks1004 <anuj.singh100420@gmail.com>
2024-05-23 01:06:22 +0530

    Project completed successfully

--- color-generator/src/SingleColor.jsx
+++ color-generator/src/SingleColor.jsx
@@ -1,8 +1,22 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ color, index }) => {
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${color.hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        toast.error("Failed to copy to clipboard");
+      }
+    }
+  };
+
   return (
     <article
       className={index > 10 ? "color color-light" : "color"}
       style={{ background: `#${color.hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{color.weight}%</p>
       <p className="color-value">#{color.hex}</p>
