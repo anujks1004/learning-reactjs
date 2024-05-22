23dd6229f0e921f101d97956a542567ee517c3d5
anujks1004 <anuj.singh100420@gmail.com>
2024-05-23 00:42:38 +0530

    Shades and tint added along with text

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Form from "./Form";
+import Values from "values.js";
+import ColorList from "./ColorList";
 
 const App = () => {
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+
   return (
     <main>
       <Form />
+      <ColorList colors={colors} />
     </main>
   );
 };
--- color-generator/src/SingleColor.jsx
+++ color-generator/src/SingleColor.jsx
@@ -1,4 +1,12 @@
-const SingleColor = () => {
-  return <div>SingleColor</div>;
+const SingleColor = ({ color, index }) => {
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${color.hex}` }}
+    >
+      <p className="percent-value">{color.weight}%</p>
+      <p className="color-value">#{color.hex}</p>
+    </article>
+  );
 };
 export default SingleColor;
