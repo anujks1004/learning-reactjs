5dc14277c4875295fe3090a6deedcb1bf6d9f15a
anujks1004 <anuj.singh100420@gmail.com>
2024-05-23 01:02:02 +0530

    Toast api used to depict error and color shown as per input value

--- color-generator/src/App.jsx
+++ color-generator/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import Form from "./Form";
 import Values from "values.js";
 import ColorList from "./ColorList";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
-
+  const addColor = (color) => {
+    try {
+      let newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
