688a55ddf31f9f713a398b186469f232404dd693
anujks1004 <anuj.singh100420@gmail.com>
2024-05-26 00:47:42 +0530

    Toastify added for alerts and project implemented

--- grocery-bud/src/App.jsx
+++ grocery-bud/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./Form";
 import Items from "./Items";
 import { nanoid } from "nanoid";
+import { ToastContainer, toast } from "react-toastify";
 
 const getLocalStorage = () => {
   let list = localStorage.getItem("list");
@@ -26,18 +27,33 @@ const App = () => {
     const newItems = [...items, newItem];
     setLocalStorage(newItems);
     setItems(newItems);
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setLocalStorage(newItems);
     setItems(newItems);
+    toast.success("item removed from the list");
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setLocalStorage(newItems);
+    setItems(newItems);
   };
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
