d56ee7d1d0b3d1e6bd5531f4bbfaaefc0a276cd6
anujks1004 <anuj.singh100420@gmail.com>
2024-05-29 00:33:17 +0530

    Components created and styling done for navbar and hero component

--- strapi/src/context.jsx
+++ strapi/src/context.jsx
@@ -4,21 +4,16 @@ const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
-
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-
   return (
-    <AppContext.AppProvider
-      value={{ isSidebarOpen, openSidebar, closeSidebar }}
-    >
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
       {children}
-    </AppContext.AppProvider>
+    </AppContext.Provider>
   );
 };
 
